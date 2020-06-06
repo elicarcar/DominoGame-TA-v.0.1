39548175c67b1e56cf645257ffb5efefcc5daf6e
elicarcar <elisabetta.carcar@gmail.com>
2020-06-06 15:48:44 +0200

    Fixes class names and ids

--- domino.js
+++ domino.js
@@ -2,7 +2,6 @@ const stock = [];
 const gamingBoard = [];
 const player1 = { name: "player1", hand: [], turn: false };
 const player2 = { name: "player2", hand: [], turn: false };
-let buttonVal = "";
 let boardNumbers = [];
 
 function generateStock() {
@@ -23,32 +22,6 @@ function generateStock() {
   st.addEventListener("click", () => drawATile());
 }
 
-// -- DONE --
-
-function updateList(list, player) {
-  const listToUpdate = document.getElementById(list);
-  while (listToUpdate.firstChild) {
-    listToUpdate.removeChild(listToUpdate.lastChild);
-  }
-
-  for (let [i, v] of player.hand.entries()) {
-    createAndAppendElem(
-      "li",
-      player.name,
-      "li--tile",
-      v.toString().replace(",", "|"),
-      i
-    );
-  }
-}
-
-function updateStock() {
-  const stockBlock = document.getElementById("stock");
-  stockBlock.innerText = `Stock ${stock.length}`;
-}
-
-// -- DONE --
-
 function shuffleTiles() {
   let currentIndex = stock.length;
   let tempIndex;
@@ -62,10 +35,8 @@ function shuffleTiles() {
     stock[randomizedIndex] = tempIndex;
   }
 
-  temporaryElement("Tiles are shuffled.", "alert--info");
+  displayTemporaryElement("Tiles are shuffled.", "alert--info");
 }
-
-// -- DONE --
 
 function distributeDominoes() {
   let i = 0;
@@ -82,7 +53,7 @@ function distributeDominoes() {
     createAndAppendElem(
       "li",
       player1.name,
-      "li--tile",
+      "tile-items",
       v.toString().replace(",", "|"),
       i
     );
@@ -92,37 +63,82 @@ function distributeDominoes() {
     createAndAppendElem(
       "li",
       player2.name,
-      "li--tile",
+      "tile-items",
       v.toString().replace(",", "|"),
       i
     );
   }
 
   const boardTable = document.querySelector(".board");
-  boardTable.classList.remove("board--display-none");
+  boardTable.classList.remove("board_display-none");
+}
+
+function updateList(list, player) {
+  const listToUpdate = document.getElementById(list);
+  while (listToUpdate.firstChild) {
+    listToUpdate.removeChild(listToUpdate.lastChild);
+  }
+
+  for (let [i, v] of player.hand.entries()) {
+    createAndAppendElem(
+      "li",
+      player.name,
+      "tile-items",
+      v.toString().replace(",", "|"),
+      i
+    );
+  }
+}
+
+function updateStock() {
+  const stockBlock = document.getElementById("stock");
+  stockBlock.innerText = `Stock ${stock.length}`;
+}
+
+function drawATile() {
+  const newTile = stock.shift();
+  const playerCantPlay =
+    !newTile.includes(boardNumbers[0]) && !newTile.includes(boardNumbers[1]);
+
+  if (!stock.length && playerCantPlay) {
+    const gameOver = document.querySelector(".container_game-over");
+    gameOver.style.display = "block";
+  }
+
+  if (playerCantPlay) {
+    displayTemporaryElement("Draw again", "alert_danger");
+  } else {
+    displayTemporaryElement("You can play now.", "alert_success");
+    isButtonDisabled("stock", true);
+  }
+
+  updateStock(`Stock ${stock.length}`);
+  if (player1.turn) {
+    player1.hand.push(newTile);
+    updateList(player1.name, player1);
+  } else {
+    player2.hand.push(newTile);
+    updateList(player2.name, player2);
+  }
 }
 
 function startTheGame() {
   generateStock();
   shuffleTiles();
   distributeDominoes();
-  disableBtn("game-start", true);
-  disableBtn("stock", true);
-  if (!gamingBoard.length) {
-    const initialTile = stock.shift();
-    gamingBoard.unshift(initialTile);
-    const leftNum = gamingBoard[0][0];
-    const rightNum = gamingBoard[gamingBoard.length - 1][1];
-    boardNumbers = [leftNum, rightNum];
-    writeProcessOnBoard(`The game started. The board is <${gamingBoard}>`);
-    console.log(`The game started. The board is <${gamingBoard}>`);
-    player1.turn = true;
-    document.getElementById("player2").classList.add("list--visibility");
-  }
-  updateStock();
-}
+  isButtonDisabled("game-start", true);
+  isButtonDisabled("stock", true);
 
-// -- DONE --
+  const initialTile = stock.shift();
+  gamingBoard.unshift(initialTile);
+  updateStock();
+  const leftNum = gamingBoard[0][0];
+  const rightNum = gamingBoard[gamingBoard.length - 1][1];
+  boardNumbers = [leftNum, rightNum];
+  document.getElementById("player2").classList.add("list_visibility");
+  player1.turn = true;
+  writeProcessOnBoard(`The game started. The board is <${gamingBoard}>`);
+}
 
 function gamePlay(player, board, selectedTile, inx) {
   let playableTiles = [];
@@ -136,11 +152,11 @@ function gamePlay(player, board, selectedTile, inx) {
   if (!playableTiles.length) {
     //check stock
     if (stock.length) {
-      temporaryElement(
+      displayTemporaryElement(
         `${player.name} can't play. They will draw a tile`,
         "alert_danger"
       );
-      disableBtn("stock", false);
+      isButtonDisabled("stock", false);
       return;
     } else {
       const gameOver = document.querySelector(".container_game-over");
@@ -152,16 +168,15 @@ function gamePlay(player, board, selectedTile, inx) {
   const playerHasTile = player.hand.includes(selectedTile);
 
   if (!playerHasTile || selectedTile === undefined) {
-    temporaryElement(
+    displayTemporaryElement(
       `This is not your tile. Please select one from yours!`,
       "alert_danger"
     );
 
     return;
   }
-  console.log(selectedTile);
 
-  // in case of selectiing unplayable tile
+  // in case of selecting unplayable tile
   const isNoMatch =
     selectedTile[0] !== board[0] &&
     selectedTile[0] !== board[1] &&
@@ -169,7 +184,7 @@ function gamePlay(player, board, selectedTile, inx) {
     selectedTile[1] !== board[1];
 
   if (isNoMatch) {
-    temporaryElement(
+    displayTemporaryElement(
       "You cannot play this tile. Please select another one!",
       "alert_danger"
     );
@@ -180,16 +195,10 @@ function gamePlay(player, board, selectedTile, inx) {
     gamingBoard.push(selectedTile);
     player.hand.splice(inx, 1);
     updateList(player.name, player);
-    console.log(
-      `${player.name} played ${selectedTile}. The board is now ${gamingBoard}`
-    );
   } else if (selectedTile[1] === board[0] && selectedTile[1] === board[1]) {
     gamingBoard.unshift(selectedTile);
     player.hand.splice(inx, 1);
     updateList(player.name, player);
-    console.log(
-      `${player.name} played ${selectedTile}. The board is now ${gamingBoard}`
-    );
   } else if (selectedTile[0] === board[0] && selectedTile[0] !== board[1]) {
     const reversedTile = selectedTile.reverse();
     gamingBoard.unshift(reversedTile);
@@ -216,10 +225,14 @@ function gamePlay(player, board, selectedTile, inx) {
 
   setTimeout(() => {
     writeProcessOnBoard(`${player.name} played ${selectedTile}.`);
-    writeProcessOnBoard(`The board is now ${boardItems}`);
+    writeProcessOnBoard(`The board is now ${boardItems}.`);
   }, 100);
 
   if (!player.hand.length) {
+    const restartButton = document.getElementById("restart-win");
+    const h4 = document.createElement("h4");
+    h4.innerText = `${player.name} wins the game!`;
+    restartButton.insertAdjacentElement("beforeend", h4);
     const gameOver = document.querySelector(".container-win");
     gameOver.style.display = "block";
   }
@@ -227,13 +240,25 @@ function gamePlay(player, board, selectedTile, inx) {
   if (player === player1) {
     player1.turn = false;
     player2.turn = true;
-    document.getElementById("player1").classList.add("list--visibility");
-    document.getElementById("player2").classList.remove("list--visibility");
+    document.getElementById("player1").classList.add("list_visibility");
+    document.getElementById("player2").classList.remove("list_visibility");
   } else {
     player1.turn = true;
     player2.turn = false;
-    document.getElementById("player2").classList.add("list--visibility");
-    document.getElementById("player1").classList.remove("list--visibility");
+    document.getElementById("player2").classList.add("list_visibility");
+    document.getElementById("player1").classList.remove("list_visibility");
+  }
+}
+
+function playerStartsToPlay(event) {
+  const index = event.target.id;
+  const leftNum = gamingBoard[0][0];
+  const rightNum = gamingBoard[gamingBoard.length - 1][1];
+  boardNumbers = [leftNum, rightNum];
+  if (player1.turn) {
+    gamePlay(player1, boardNumbers, player1.hand[index], index);
+  } else {
+    gamePlay(player2, boardNumbers, player2.hand[index], index);
   }
 }
 
@@ -251,12 +276,12 @@ function createAndAppendElem(child, parent, className, content, id = "") {
 function writeProcessOnBoard(content) {
   const boardDiv = document.querySelector(".board");
   const p = document.createElement("p");
-  p.classList.add("board--game-process");
+  p.classList.add("board_game-process");
   p.innerText = content;
   boardDiv.insertAdjacentElement("beforeend", p);
 }
 
-function temporaryElement(text, alertType) {
+function displayTemporaryElement(text, alertType) {
   const infoContainer = document.querySelector(".container_info");
   const newDiv = document.createElement("div");
   newDiv.setAttribute("id", "inquirer");
@@ -267,58 +292,9 @@ function temporaryElement(text, alertType) {
   setTimeout(function () {
     const tempDiv = document.getElementById("inquirer");
     tempDiv.remove();
-  }, 3000);
+  }, 1000);
 }
 
-function disableBtn(id, bool) {
+function isButtonDisabled(id, bool) {
   document.getElementById(id).disabled = bool;
-}
-
-// FIXME
-
-function clicked_btn() {
-  const question = document.querySelector(".container_question");
-  const button = document.getElementById(this).value;
-  console.log(button);
-  // question.style.display = "none";
-}
-
-function drawATile() {
-  const newTile = stock.shift();
-
-  if (!stock.length) {
-    const gameOver = document.querySelector(".container_game-over");
-    gameOver.style.display = "block";
-  }
-
-  if (
-    !newTile.includes(boardNumbers[0]) &&
-    !newTile.includes(boardNumbers[1])
-  ) {
-    temporaryElement("Draw again", "alert_danger");
-  } else {
-    temporaryElement("You can play now.", "alert_success");
-    disableBtn("stock", true);
-  }
-
-  updateStock(`Stock ${stock.length}`);
-  if (player1.turn) {
-    player1.hand.push(newTile);
-    updateList(player1.name, player1);
-  } else {
-    player2.hand.push(newTile);
-    updateList(player2.name, player2);
-  }
-}
-
-function getTileValue(event) {
-  const index = event.target.id;
-  const leftNum = gamingBoard[0][0];
-  const rightNum = gamingBoard[gamingBoard.length - 1][1];
-  boardNumbers = [leftNum, rightNum];
-  if (player1.turn) {
-    gamePlay(player1, boardNumbers, player1.hand[index], index);
-  } else {
-    gamePlay(player2, boardNumbers, player2.hand[index], index);
-  }
 }
